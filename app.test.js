801b44c5fb05089d53aae3d44cf7510c290e0f33
Daniela G <daniela.gruberova@merck.com>
2018-07-24 17:29:26 +0200

    fixed tests version

--- app.test.js
+++ app.test.js
@@ -2,7 +2,6 @@ import React from 'react'
 import App from './App.js'
 import {shallow} from 'enzyme'
 
-
 describe('Phone words app', () => {
 
   it('should render single page app', () => {
@@ -13,59 +12,9 @@ describe('Phone words app', () => {
       const textInput = wrapper.find('TextInput')
       const button = wrapper.find('Button')
       const scrollView = wrapper.find('ScrollView')
-      // Text behaves weird....
 
       expect(textInput).toHaveLength(1)
       expect(button).toHaveLength(1)
       expect(scrollView).toHaveLength(1)
-  })
-
-  it('should validate input', () => {
-    // GIVEN
-    const validateInput = jest.fn()
-    const inputMock = "3564"
-    const wrapper = shallow(
-      <App
-        validateInput={validateInput}
-      />
-    )
-
-    // WHEN
-      const result = validateInput(inputMock)
-
-    //THEN
-    expect(result).toBeTruthy
-  })
-
-  it('should throw an error on ivalid input', () => {
-    // GIVEN
-    const validateInput = jest.fn()
-    const inputMock = "0356a"
-    const wrapper = shallow(
-      <App
-        validateInput={validateInput}
-      />
-    )
-    // WHEN
-    const result = validateInput(inputMock)
-
-    //THEN
-    expect(result).toBeFalsy
-  })
-
-  it('process the input and return possible combinations and words', () => {
-    // GIVEN
-    const getCombinationsAndFilterWords = jest.fn()
-    const inputMock = "356"
-    const wrapper = shallow(
-      <App
-        getCombinationsAndFilterWords={getCombinationsAndFilterWords}
-      />
-    )
-    // WHEN
-      const result = getCombinationsAndFilterWords(inputMock)
-
-    //THEN
-    expect(result).toBeTruthy
   })
 })
